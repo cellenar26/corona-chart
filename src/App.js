f3f51443504dcd57ee301451710f40b4a824efff
cellenar26 <cellenar26@gmail.com>
2021-09-22 14:40:09 +0900

    install axios and react-chartjs-2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Contents from './components/Contents';
 
 function App() {
   return (
-    <div>hi</div>
+    <div className="app">
+      <Header />
+      <Contents />
+    </div>
   );
 }
 
